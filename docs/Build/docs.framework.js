25e4ef01b0ef5481dbaf55d8a6de24e3e8d33284
Ray Liu公司電腦 <coolray@gmail.com>
2026-05-07 16:34:48 +0800

    放回大檔

--- docs/Build/docs.framework.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:07c379465c63a59069d8b9f11717d881dfaadc1fc864a832958c19b72cfad34a
-size 751491
--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:15a4a6b5f92bd34853ef82db5344d93d7486ec4b038436ba93458174f6908e97
-size 104
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+});
+
